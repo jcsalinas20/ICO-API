9acafa9117ef41a926327b88129feaf1da9758cc
jcsalinas20 <jcsalinasn20@gmail.com>
2020-02-26 00:14:30 +0100

    cambios index

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # ICO-API
 
+## npm run dev
+
+para iniciar el programa en dev.
+
 ## npm start
 
-para iniciar el programa.
+para iniciar el programa en pro.
--- index.js
+++ index.js
@@ -1,47 +1,70 @@
-var express = require('express');
-var http = require('http');
-require('dotenv').config();
+var express = require("express");
+var MongoClient = require("mongodb");
+var http = require("http");
+require("dotenv").config({ path: '.env'});
 var app = express();
+
+// Connect to the MongoDB cluster
+// MongoClient.connect(process.env.HOST, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
 
 /*
     MÉTODO PARA AUTENTICAR UN USUARIO:
     RECIBE UN USUARIO Y UNA PASSWORD.
     DEVUELVE TRUE SI EL USUARIO EXISTE Y FALSE SI NO ES CORRECTO.
 */
-app.get('/paciente/auth/:username/:password', (req, res) => {
-  var usuario = req.params.username;
-  var password = req.params.password;
-  res.send(true)
-});
+app.get("/paciente/auth/:username/:password", (req, res) => {
+    var usuario = req.params.username
+    var password = req.params.password
+    res.send(true)
+})
 
 /*
     MÉTODO PARA PREGUNTAR LA MEDICACION DE UN PACIENTE.
     RECIBE UN USUARIO.
     DEVUELVE UN ARRAY CON LA INFORMACION DE LA MEDICACIÓN.
 */
-app.get('/paciente/medicacion/:username', (req, res) => {
-  var usuario = req.params.username;
-  var medicacion = ['Ibuprofeno', 'Paracetamol', 'Dormidina'];
-  res.send(medicacion)
-});
+app.get("/paciente/medicacion/:username", (req, res) => {
+    var usuario = req.params.username
+    var medicacion = ["Ibuprofeno", "Paracetamol", "Dormidina"]
+    res.send(medicacion)
+})
 
 /*
     MÉTODO PARA PREGUNTAR LAS CONSULTAS DE UN PACIENTE.
     RECIBE UN USUARIO.
     DEVUELVE UN ARRAY CON LA INFORMACION DE LAS CONSULTAS.
 */
-app.get('/paciente/consultas/:username', (req, res) => {
-  var usuario = req.params.username;
-  var consultas = ['Miercoles 8 de enero', 'Martes 8 de febrero', 'Jueves 8 de marzo'];
-  res.send(consultas)
+app.get("/paciente/consultas/:username", (req, res) => {
+    var usuario = req.params.username;
+    var consultas = [
+        "Miercoles 8 de enero",
+        "Martes 8 de febrero",
+        "Jueves 8 de marzo"
+    ];
+    res.send(consultas);
 });
 
 //MENSAJE QUE VE EL USUARIO CUANDO ENTRA A LA URL
-app.get('/', (req, res) => {
-  res.status(200).send("<h1>API REST OPERATIVO</h1>")
-})
+app.get("/", (req, res) => {
+    res.status(200).send("<h1>API REST OPERATIVO</h1>")
+});
+
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 3000;
 
 //INICIO EL SERVIDOR
-http.createServer(app).listen(process.env.PORT | 3000, () => {
-  console.log('Se ha iniciado el servidor en el puerto ' + process.env.PORT);
+// app.listen(port, host, () => {
+//     console.log("Example app listening on http://localhost:3000 !")
+// });
+MongoClient.connect(process.env.HOST, function( err, _client ) {
+    // si no ens podem connectar, sortim
+    if( err ) throw err;
+    mongoClient = _client;
+    // si no hi ha cap error de connexió, engeguem el servidor
+    app.listen(port, host, () => {
+        console.log('Example app listening on http://localhost:3000 !');
+    });
 });
