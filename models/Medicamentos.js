8e623dd5ab638b006f3a75669b4d169705523979
jcsalinas20 <jcsalinasn20@gmail.com>
2020-02-27 20:43:38 +0100

    Models.Medicamentos echo

---
+++ models/Medicamentos.js
@@ -0,0 +1,15 @@
+const mongo = require('mongoose');
+const Schema = mongo.Schema;
+
+const medicamentosSchema = new Schema({
+    id: {
+        type: 'Number',
+        trim: true
+    },
+    nombre: {
+        type: 'String',
+        trim: true
+    }
+});
+
+module.exports = mongo.model('Medicamentos', medicamentosSchema);
