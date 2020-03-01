75169e8f24c20ee3ae93217ab2549a40346b63b6
jcsalinas20 <jcsalinasn20@gmail.com>
2020-03-01 19:10:00 +0100

    Access-Control-Allow: true

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ const port = process.env.PORT || 3000
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Credentials", true)
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS")
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+    )
+    next()
+})
 
 mongo.Promise = global.Promise
 mongo.connect(
